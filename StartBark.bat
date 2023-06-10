c63a6a07dad5ae8fa1347c8c8c298b320b9b0819
C0untFloyd <131583554+C0untFloyd@users.noreply.github.com>
2023-06-10 10:40:02 +0200

    Funcy/linkify requirement git pull shouldn't be turned on by default

--- StartBark.bat
+++ StartBark.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Remove the REM from the next line to stay up-to-date
-git pull
+REM git pull
 REM set COMMANDLINE_ARGS=-smallmodels -autolaunch -forcecpu
 set COMMANDLINE_ARGS=-autolaunch
 python webui.py %COMMANDLINE_ARGS%
